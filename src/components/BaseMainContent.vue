33860fe771ecaa8fca92854a33023f305d1f4d02
Mogeum <kimmogeum@dobedub.com>
2025-04-02 16:38:10 +0900

    메인화면 텍스트 추가

---
+++ src/components/BaseMainContent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="base-title" :class="{ 'wide': props.isWide }">
+    <p
+        :style="{
+        color: props.textColor,
+        fontSize: `${props.fontSize}px`
+      }"
+    >
+      {{ props.title }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  textColor: {
+    type: String,
+    default: '#ffffff'
+  },
+  fontSize: {
+    type: Number,
+    default: 40
+  },
+  isWide: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<style scoped>
+.base-title {
+  padding-bottom: 50px;
+  &.wide {
+    padding-bottom: 160px;
+  }
+  p {
+    margin: 0;
+    line-height: 1;
+    font-weight: 900;
+  }
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,9 @@
 <template>
   <q-page class="flex">
     <div class="main-page">
-      <q-img src="/icons/Group-2613907.png" class="main-image" > </q-img>
+      <div class="main-image-inner">
+        <img src="/icons/Group-2613907.png" class="main-image" />
+      </div>
       <div class="main-data">
         <div class="main-data-header">
           <q-img src="/icons/pickme-logo.png" style="width: 63px;"/>
@@ -16,6 +18,22 @@
           :gap="15"
           @item-click="handleItemClick"
         />
+        <div class="inner-wrapper main-data-txt text-center ">
+          <div class="main-data-txt-chip flex flex-center">
+            <span class="custom-font chip ">감성</span>
+            <span class="custom-font">+</span>
+            <span class="custom-font chip">상상</span>
+            <span class="custom-font">+</span>
+            <span class="custom-font chip">현실감각</span>
+          </div>
+          <p class="custom-font text-main-white q-mt-md q-mb-lg" style="font-size: 20px" >모두 충전되는 오감 몰입형 엔터테인먼트가 시작된다</p>
+          <div class="text-white main-text-infor">
+            <p>지금까지와는 전혀 다른 팬층을 만날 수 있는 기회</p>
+            <p>웹툰 속 캐릭터로 사랑받는 당신,</p>
+            <p>화보·더빙·숏폼으로 확장되는 IP 자산,</p>
+            <p>그리고 콘텐츠마다 각인되는 목소리의 아이덴티티</p>
+          </div>
+        </div>
       </div>
     </div>
     <ProductDialog
@@ -28,8 +46,7 @@
 import BaseCarousel from "components/BaseCarousel.vue";
 import MainData from "src/json/MainData.json"
 import ProductDialog from "src/dialogs/ProductDialog.vue";
-import {ref} from "vue";
-import { computed } from 'vue'
+import {ref, onMounted, computed} from "vue";
 
 const showProductDialog = ref(false)
 const selectedItem = ref(null)
@@ -46,7 +63,9 @@ const filteredInfluencers = computed(() => {
 //   selectedItem.value = ProductData;
 //   showProductDialog.value = true
 // }
+onMounted(()=> {
 
+})
 </script>
 <style scoped>
 .main-page {
@@ -59,9 +78,11 @@ const filteredInfluencers = computed(() => {
 }
 .main-image{
   margin-top: 70px;
-  max-width: 400px;
+  max-width: 100%;
   max-height: 656px;
+  object-fit: contain !important;
 }
+
 .main-data{
   width: 516px !important;
   overflow: hidden;
@@ -71,12 +92,33 @@ const filteredInfluencers = computed(() => {
     padding: 17px 30px;
   }
 }
+.main-text-infor p{
+  margin: 8px 0;
+}
+.main-data-txt-chip {
+  color: #FF2D55 !important;
+
+}
+.custom-font.chip{
+  display: inline-block;
+  width: 100px;
+  height: 50px;
+  margin: 0 5px;
+  font-size: 20px;
+  line-height: 50px;
+  text-align: center;
+  border: 1px solid #FF2D55;
+  border-radius: 25px;
+}
 @media screen and (max-width: 960px) {
   .main-page {
     align-items: center;
     justify-content: flex-start;
     flex-direction: column;
     gap: 0;
+  }
+  .main-image-inner{
+    padding: 0 18px;
   }
 }
 
@@ -89,6 +131,18 @@ const filteredInfluencers = computed(() => {
       height: 60px;
       padding: 17px 30px;
     }
+  }
+}
+@media screen and (max-width: 400px) {
+  .custom-font.chip{
+    display: inline-block;
+    width: 75px;
+    height: 40px;
+    font-size: 16px;
+    line-height: 40px;
+    text-align: center;
+    border: 1px solid #FF2D55;
+    border-radius: 25px;
   }
 }
 </style>
